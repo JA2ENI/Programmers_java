eb47539832602c8f6a63d0366049f373e2d1deb6
JA2ENI <73980078+JA2ENI@users.noreply.github.com>
2023-06-23 23:01:40 +0900

    [level 0] Title: 두 수의 차, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int check = -50000;
+        int check2 = 50000;
+        int answer = 0;
+        
+        if(check<=num1 && check2>=num1 && check<=num2 && check2>=num2) {
+            answer = num1 - num2;
+        }
+        return answer;
+    }
+}
